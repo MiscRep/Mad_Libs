2b5a49f9f4db8b2f23af45108229b541a5b37fa6
Ebony B <70521133+MiscRep@users.noreply.github.com>
2022-07-08 01:44:38 -0400

    Start
    This is the start of the code

---
+++ MadLibs.java
@@ -0,0 +1,8 @@
+public class MadLibs {
+    //This will be a very simple game of Mad Libs
+
+        public static void main (String[] args){
+
+            //The template for the story
+        }
+}
